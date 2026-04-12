78d7431f1c188fe6a05ce3b25b74336fe7957545
Nawaf <ntayyb@gmail.com>
2026-04-12 17:24:19 +0200

    Fix merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,4 @@ createRoot(document.getElementById('root')).render(
             <App />
         </Provider>
     </StrictMode>,
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> f9e297bea3be80f7ee5ab6727ff0fc1f6d2ef13a
